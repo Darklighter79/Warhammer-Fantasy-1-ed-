2233353b2d428e70009bb7bf2c616c23bebf58c6
Darklighter79 <darklighter1979@gmail.com>
2020-10-05 11:18:28 +0200

    Update

--- Skill_roll.py
+++ Skill_roll.py
@@ -3,30 +3,17 @@ import random
 n = int(input("Enter your skill value: "))
 a = int(input("Enter the number of actions: "))
 
-for roll in range (0, a):
+for roll in range(0, a):
 
-    roll = random.randint(1, 100)
+    roll = random.randint(1, 5)
 
-    if roll in range(96, 100) and n < roll and roll in range(0, a):
+    if roll in range(96, 100) and n < roll:
         print(
-            f"You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill")
-    elif roll in range(1, 4) and n > roll:
-        print(f"You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill")
+            f"You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill.")
+    elif roll in range(1, 5) and n > roll:
+        print(
+            f"You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill.")
     elif roll <= n:
-        print(f"You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill")
+        print(f"You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill.")
     elif roll > n:
-        print(f"You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print(f"You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill.")
