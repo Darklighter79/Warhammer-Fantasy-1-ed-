3face32c8e7b03ae01794bc40324ecf09ef73a86
Darklighter79 <darklighter1979@gmail.com>
2020-10-05 11:08:04 +0200

    Update

--- Skill_roll.py
+++ Skill_roll.py
@@ -1,16 +1,32 @@
 import random
 
 n = int(input("Enter your skill value: "))
+a = int(input("Enter the number of actions: "))
 
-roll = random.randint(1, 100)
-print(f"Skill value is {n}. You rolled {roll}.")
+for roll in range (0, a):
 
-if roll in range(96, 100) and n < roll:
-    print(
-        f"Critical failure. Test failed by: {roll - n}.That is {roll // n} of your skill")
-elif roll in range(1, 4) and n > roll:
-    print(f"Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill")
-elif roll <= n:
-    print(f"Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill")
-elif roll > n:
-    print(f"Test failed by: {roll - n}. That is {roll // n} of your skill")
+    roll = random.randint(1, 100)
+
+    if roll in range(96, 100) and n < roll and roll in range(0, a):
+        print(
+            f"You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill")
+    elif roll in range(1, 4) and n > roll:
+        print(f"You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill")
+    elif roll <= n:
+        print(f"You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill")
+    elif roll > n:
+        print(f"You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
