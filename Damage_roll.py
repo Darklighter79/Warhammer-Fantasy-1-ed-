d242685204294dc4b17104a720d92102c693a30f
Darklighter79 <darklighter1979@gmail.com>
2020-10-05 21:53:40 +0200

    Update

--- Damage_roll.py
+++ Damage_roll.py
@@ -1,14 +1,15 @@
 import random
 
 n = int(input("Input your strength value with all additional modifiers: "))
+a = int(input("Input your attack value: "))
 
-roll = random.randint(1, 6)
+for _ in range(a):
+    roll = random.randint(1, 6)
+    lst_num = [roll]
 
-lst_num = [roll]
-
-if roll == 6:
-    while lst_num[-1] == 6:
-        lst_num.append(random.randint(1, 6))
-    print(f'Fury of Ulric! Additional damage!Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
-else:
-    print(f'Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
+    if roll == 6:
+        while lst_num[-1] == 6:
+            lst_num.append(random.randint(1, 6))
+        print(f'Fury of Ulric! Additional damage!Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
+    else:
+        print(f'Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
