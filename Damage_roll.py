75197d1b0e6f1dd9b2d400cb921b1a86635935b6
Rafal <darklighter79@gmail.com>
2020-10-22 12:56:14 +0200

    Update

--- Damage_roll.py
+++ Damage_roll.py
@@ -3,13 +3,13 @@ import random
 n = int(input("Input your strength value with all additional modifiers: "))
 a = int(input("Input your attack value: "))
 
-for _ in range(a):
+for i in range(1, a+1):
     roll = random.randint(1, 6)
     lst_num = [roll]
 
     if roll == 6:
         while lst_num[-1] == 6:
             lst_num.append(random.randint(1, 6))
-        print(f'Fury of Ulric! Additional damage!Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
+        print(f'{i}. Fury of Ulric! Additional damage!Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
     else:
-        print(f'Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
+        print(f'{i}. Your strength total: {n} and your roll is {lst_num}. Total {n + sum(lst_num)}.')
--- Skill_roll.py
+++ Skill_roll.py
@@ -4,17 +4,21 @@ import random
 n = int(input("Enter your skill value: "))
 a = int(input("Enter the number of actions: "))
 
-for roll in range(0, a):
+crit_msg = ("Wsadź łeb pod wóz", "Po co przyjechałeś?", "Mogłeś się dać na eksperymenty w Nuln", "Pogrom! Wrogowie podejrzewają, że jesteś jedynie dywersją", "Losuj nową postać",
+            "Nie grasz przez rok", "Mike, daj końmi do tyłu", "Od dziś jesteś na liście kapłanej Shyalli jako cel do atakowania obok wyznawców Nugrla")
+
+for i in range(1, a+1):
 
     roll = random.randint(1, 100)
 
     if roll in range(96, 100) and n < roll:
         print(
-            f"You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill.")
+            f"{i}. You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill. Głos wyroczni: {random.sample(crit_msg, 1)}")
     elif roll in range(1, 6) and n > roll:
         print(
-            f"You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill.")
+            f"{i}. You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your "
+            f"skill.")
     elif roll <= n:
-        print(f"You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill.")
+        print(f"{i}. You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill.")
     elif roll > n:
-        print(f"You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill.")
+        print(f"{i}. You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill.")
