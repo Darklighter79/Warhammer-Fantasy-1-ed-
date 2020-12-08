a931fdc59bdcaed3733a9fec9f41341c8caa4659
RavDark1979 <darklighter1979@gmail.com>
2020-12-08 09:26:21 +0100

    Update

--- Skill_roll.py
+++ Skill_roll.py
@@ -4,13 +4,20 @@ import random
 n = int(input("Enter your skill value: "))
 a = int(input("Enter the number of actions: "))
 
+crit_msg = ["Gods are not on your side today",
+            "Do you have spare character sheets ready?",
+            "Maybe try to bargain with GM?",
+            "Time for Destiny Point?",
+            "I got a bad feeling about this..."]
+
 for i in range(1, a+1):
 
-    roll = random.randint(1, 100)
+    roll = random.randint(95, 100)
 
-    if roll in range(96, 100) and n < roll:
+    if roll in range(96, 101) and n < roll:
         print(
-            f"{i}. You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill.")
+            f"{i}. You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill."
+            f"The voice of Oracle: {random.choice(crit_msg)}")
     elif roll in range(1, 6) and n > roll:
         print(
             f"{i}. You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your "
