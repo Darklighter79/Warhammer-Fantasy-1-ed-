6fc26c83e7a6c54c90539466df2adfaf5b4ce11b
Darklighter79 <darklighter1979@gmail.com>
2021-12-17 19:59:41 +0100

    Update

--- Skill_roll.py
+++ Skill_roll.py
@@ -1,30 +1,42 @@
 #skill test
-import random
 
-n = int(input("Enter your skill value: "))
-a = int(input("Enter the number of actions: "))
+def skillroll():
 
-crit_msg = ("Gods turned their attention away for a moment...",
-            "You almost had it...almost",
-            "Always look on the bright side of...warp",
-            "Bring out yer dead!",
-            "Do you have spare characted sheet ready?",
-            "Dice do not like you I guess...",
-            "Try to bargain with Game Master...",
-            "Chaos gods unleashed their fury!")
+    import random
 
-for i in range(1, a+1):
+    n = int(input("Enter your skill value: "))
+    a = int(input("Enter the number of actions: "))
 
-    roll = random.randint(1, 100)
+    crit_msg = ("Gods turned their attention away for a moment...",
+                "You almost had it...almost",
+                "Always look on the bright side of...warp",
+                "Bring out yer dead!",
+                "Do you have spare characted sheet ready?",
+                "Dice do not like you I guess...",
+                "Try to bargain with Game Master...",
+                "Chaos gods unleashed their fury!")
 
-    if roll in range(96, 100) and n < roll:
-        print(
-            f"{i}. You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill. Voice of oracle: {random.sample(crit_msg, 1)}")
-    elif roll in range(1, 6) and n > roll:
-        print(
-            f"{i}. You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your "
-            f"skill.")
-    elif roll <= n:
-        print(f"{i}. You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill.")
-    elif roll > n:
-        print(f"{i}. You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill.")
+    for i in range(1, a+1):
+
+        roll = random.randint(95, 100)
+
+        if roll in range(96, 101) and n < roll:
+            print(
+                f"{i}. You rolled: {roll}. Critical failure! Test failed by: {roll - n}.That is {roll // n} of your skill. Voice of oracle: {random.sample(crit_msg, 1)}")
+        elif roll in range(1, 6) and n > roll:
+            print(
+                f"{i}. You rolled: {roll}. Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your "
+                f"skill.")
+        elif roll <= n:
+            print(f"{i}. You rolled: {roll}. Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill.")
+        elif roll > n:
+            print(f"{i}. You rolled: {roll}. Test failed by: {roll - n}. That is {roll // n} of your skill.")
+
+    again = input("Again? 'y' to continue  ")
+    if again == "y":
+        skillroll()
+    else:
+        print("May the Sigmar guide you")
+
+skillroll()
+
